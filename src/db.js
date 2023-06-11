43739ae1098be0e282d3fc16f911be9253e6ceaf
unknown <dileep8417@gmail.com>
2023-06-11 20:08:56 +0530

    env variable changes

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/mailTracker');
+        const db_uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mailTracker';
+        await mongoose.connect(db_uri);
         console.log('Database connected');
     } catch (e) {
         console.log(e.message);
